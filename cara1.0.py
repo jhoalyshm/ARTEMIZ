24a7c5f626ec8318afb39c41e92c617f0bec9c08
jhoalyshm <61716802+jhoalyshm@users.noreply.github.com>
2020-03-02 21:12:23 -0300

    Add files via upload

---
+++ cara1.0.py
@@ -0,0 +1,11 @@
+#ESCRIBA EL PREGRAMAIMPRIME CARA, QUE IMPRIMEUNA CARA,UTILICE LOS CARACTERES DEL TEXTO
+#JORGE ALBERTO ICHUTA TUCO   C.I.  10037921 LP
+print("             /////////   ") #cabello
+print("           ///////////    ")#cabello
+print("           ¬¬¬¬¬¬¬¬¬¬    ")
+print("          | ---  --- |    ")#cejas
+print("         (| (O)  (O) |)   ")#ojos y orejas
+print("          |    /\    |    ")#nariz 
+print("          |    ^^    |    ")#nariz
+print("           \  <-->  /     ")#boca
+print("            \______/      ")
